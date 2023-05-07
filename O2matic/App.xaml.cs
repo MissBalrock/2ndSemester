c7c3597182a441e507948e1544989ebe78119b78
Anna Anuoluwapo Ajayi <aaaj48595@edu.ucl.dk>
2023-05-07 21:41:37 +0200

    Ændret startup og sat lidt scafolding

--- O2matic/App.xaml.cs
+++ O2matic/App.xaml.cs
@@ -8,10 +8,14 @@ using System.Windows;
 
 namespace O2matic
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
