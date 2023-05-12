d69862b27f1085033ee66c14673946191479af39
StephanieGaarsmand <113356066+StephanieGaarsmand@users.noreply.github.com>
2023-05-12 10:51:57 +0200

    kodeforklaring

--- O2matic/Persistens/EquiptmentRepository.cs
+++ O2matic/Persistens/EquiptmentRepository.cs
@@ -12,42 +12,43 @@ using System.Threading.Tasks;
 
 namespace O2matic.Persistens
 {
-    public class EquiptmentRepository : IDisposable
+    public class EquiptmentRepository : IDisposable // Idispoddableinterfacet  lukker connection til database, så databasen ikke står åben til sidst. EquiptmentRepository har implemeteret Idisposabel
     {
-        public SqlConnection Connection { get; private set; }
-        public EquiptmentRepository()
+        public SqlConnection Connection { get; private set; } // hentes fra MIckrosoft.Data.SqlClient, som er et bibliotek
+        public EquiptmentRepository() 
         {
-            var dataSource = "(local)\\SQLEXPRESS";
-            var initialCatalog = "O2maticDevelopment";
+            var dataSource = "(local)\\SQLEXPRESS"; // en variabel, der er adressen til databasen.
+            var initialCatalog = "O2maticDevelopment"; // navnet på databasen
 
-            var connectionString = $"Data Source={dataSource};Initial Catalog={initialCatalog};Integrated Security=True;TrustServerCertificate=True";
-            Connection = new SqlConnection(connectionString);
-            Connection.Open();
+            var connectionString = $"Data Source={dataSource};Initial Catalog={initialCatalog};Integrated Security=True;TrustServerCertificate=True"; //Connectionstring er hele stien til at finde ind til databasen
+            Connection = new SqlConnection(connectionString); //tager variablen connectionstring og instatiere den. 
+            Connection.Open(); // databasen er nu åben
         }
 
-        public void Save(Equipment equipment)
+        public void Save(Equipment equipment) // 
         {
-            var commandString = $"INSERT INTO Equipment (EquipmentTypeID, SerialNumber, RegistrationDate, LocationID) " +
-                $"VALUES ({equipment.EquipmentTypeId}, '{equipment.SerialNumber}', {DateTime.Now}, {equipment.LocationId});";
-            var command = new SqlCommand(commandString, Connection);
+            using. // man kan inkapsle det i en using, da den ´nedarver fra en klasse som immplemetere Idisposable
+            var commandString = "INSERT INTO Equipment (EquipmentTypeID, SerialNumber, RegistrationDate, LocationID) " + // $= streng interpulation // Equiptment = tabel navnet på databasemn.// f.eks. serialnumber er navnet på en kolonne i databasen.
+                $"VALUES ({equipment.EquipmentTypeId}, '{equipment.SerialNumber}', {DateTime.Now}, {equipment.LocationId});"; // Values funktion definerer veriderne i databasen for kolonnerne. // .=henter property fra model klassen.
+            var command = new SqlCommand(commandString, Connection); // nu bliver SqlCommand istansieret og kommer ind i databasen.
 
-            command.ExecuteReader().Close();
+            command.ExecuteReader().Close(); //lukker connection til databsen/table. 
         }
 
-        public Equipment? Get(int id)
+        public Equipment? Get(int id) // returnere det udstyr du leder efter, ved at søge efter ID. // der mangler en objekt reference, som kan være null, for at tjekke om man har en værdi som reference.
         {
-            var commandString = $"SELECT * FROM dbo.Equipment WHERE ID = {id}";
-            var command = new SqlCommand(commandString, Connection);
+            var commandString = $"SELECT * FROM dbo.Equipment WHERE ID = {id}"; // {id} = kommer fra Equiptment model klassen. // Det med "rødt" er syntaksten fra databasen // *= bruges, fordi vores table er lille, og alle vores kollene navne skal bruges. 
+            var command = new SqlCommand(commandString, Connection); // nu bliver SqlCommand instiansieret og der er nu en connection til databasen, hvor udstyr bliver hentet via Id. 
 
-            using (var reader = command.ExecuteReader())
+            using (var reader = command.ExecuteReader()) // ExecuteReader læser data fra databasen. implementere Idisposabel, elles kan det ikke bruges. 
             {
-                while (reader.Read())
+                while (reader.Read()) // så længe metoden ExecuteReader metoden er igang med at læse
                 {
 
-                    var dateString = reader[3].ToString();
+                    var dateString = reader[3].ToString(); 
                     var registrationDate = DateTime.UnixEpoch;
                     if (dateString != null)
-                        registrationDate = DateTime.Parse(dateString);
+                        registrationDate = DateTime.Parse(dateString); // tjekekr om registration date er = null. // Dette har vi valgt at gøre, for at der ikke kan opstå en fejl.
 
                     var result = new Equipment(
                         id: Convert.ToInt32(reader[0].ToString()),
@@ -100,7 +101,6 @@ namespace O2matic.Persistens
 
 
             }
-        }
 
         public void Dispose()
         {
