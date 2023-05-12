bec34fea357460a29a3727fcc0a44022c121ebd0
Anna Anuoluwapo Ajayi <aaaj48595@edu.ucl.dk>
2023-05-12 10:49:54 +0200

    test

--- O2matic/Models/Equiptment.cs
+++ O2matic/Models/Equiptment.cs
@@ -8,15 +8,6 @@ namespace O2matic.Models
 {
     public class Equiptment
     {
-        public EquipmentType Type { get; set; }
-        public int SerialNumber { get; set; }
-        public int LocationId { get; set; }
 
-        public Equiptment(EquiptmentType type, int serialNumber, int locationId)
-        {
-            Type = type;
-            SerialNumber = serialNumber;
-            LocationId = locationId;
-        }
     }
 }
