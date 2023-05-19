9461061f20f6894007c375c77b59ef6736008404
Anna Anuoluwapo Ajayi <aaaj48595@edu.ucl.dk>
2023-05-19 12:46:07 +0200

    Repo med kommentar

--- O2matic/Persistens/EquiptmentRepository.cs
+++ O2matic/Persistens/EquiptmentRepository.cs
@@ -10,100 +10,106 @@ using System.Threading.Tasks;
 
 namespace O2matic.Persistens
 {
-    public class EquiptmentRepository
+    public class EquiptmentRepository : IDisposable // Idispoddableinterfacet  lukker connection til database, så databasen ikke står åben til sidst. EquiptmentRepository har implemeteret Idisposabel
     {
-        private List<Equiptment> equiptments = new List<Equiptment>();
-
+        public SqlConnection Connection { get; private set; } // hentes fra MIckrosoft.Data.SqlClient, som er et bibliotek
         public EquiptmentRepository()
         {
-            InitializeRepository();
-        }
-        private void InitializeRepository()
-        {
-            try
-            {
-                using (StreamReader sr = new StreamReader("Equiptment.csv"))
-                {
-                    String line = sr.ReadLine();
-                    while (line != null)
-                    {
-                        string[] parts = line.Split(' ');
+            var dataSource = "(local)\\SQLEXPRESS"; // en variabel, der er adressen til databasen.
+            var initialCatalog = "O2maticDevelopment"; // navnet på databasen
 
-                        this.Add(parts[0], int.Parse(parts[1]), int.Parse(parts[2]));
-                        line = sr.ReadLine();
-                    }
+            var connectionString = $"Data Source={dataSource};Initial Catalog={initialCatalog};Integrated Security=True;TrustServerCertificate=True"; //Connectionstring er hele stien til at finde ind til databasen
+            Connection = new SqlConnection(connectionString); //tager variablen connectionstring og instatiere den. 
+            Connection.Open(); // databasen er nu åben
+        }
+
+        public void Save(Equipment equipment) // 
+        {
+            // man kan inkapsle det i en using, da den ´nedarver fra en klasse som immplemetere Idisposable
+            var commandString = "INSERT INTO Equipment (EquipmentTypeID, SerialNumber, RegistrationDate, LocationID) " + // $= streng interpulation // Equiptment = tabel navnet på databasemn.// f.eks. serialnumber er navnet på en kolonne i databasen.
+                $"VALUES ({equipment.EquipmentTypeId}, '{equipment.SerialNumber}', {DateTime.Now}, {equipment.LocationId});"; // Values funktion definerer veriderne i databasen for kolonnerne. // .=henter property fra model klassen.
+            var command = new SqlCommand(commandString, Connection); // nu bliver SqlCommand istansieret og kommer ind i databasen.
+
+            command.ExecuteReader().Close(); //lukker connection til databsen/table.  
+        }
+
+        public Equipment? Get(int id) // returnere det udstyr du leder efter, ved at søge efter ID. // der mangler en objekt reference, som kan være null, for at tjekke om man har en værdi som reference.
+        {
+            var commandString = $"SELECT * FROM dbo.Equipment WHERE ID = {id}"; // {id} = kommer fra Equiptment model klassen. // Det med "rødt" er syntaksten fra databasen // *= bruges, fordi vores table er lille, og alle vores kollene navne skal bruges. 
+            var command = new SqlCommand(commandString, Connection); // nu bliver SqlCommand instiansieret og der er nu en connection til databasen, hvor udstyr bliver hentet via Id. 
+
+            using (var reader = command.ExecuteReader()) // ExecuteReader læser data fra databasen. implementere Idisposabel, elles kan det ikke bruges. 
+            {
+                while (reader.Read()) // så længe metoden ExecuteReader metoden er igang med at læse
+                {
+
+                    var dateString = reader[3].ToString();
+                    var registrationDate = DateTime.UnixEpoch;
+                    if (dateString != null)
+                        registrationDate = DateTime.Parse(dateString); // tjekekr om registration date er = null. // Dette har vi valgt at gøre, for at der ikke kan opstå en fejl.
+
+                    var result = new Equipment(
+                        id: Convert.ToInt32(reader[0].ToString()),
+                        equipmentTypeId: Convert.ToInt32(reader[1].ToString()),
+                        serialNumber: Convert.ToInt32(reader[2].ToString()),
+                        registrationDate: registrationDate,
+                        locationId: Convert.ToInt32(reader[4].ToString())
+                        //Device status
+                        );
+                    return result;
                 }
             }
-            catch (IOException)
-            {
-                throw;
-            }
+            return null;
         }
-        public Equiptment Add(string equiptmentType, int serialNumber, int Id)
-        {
-            Equiptment result = null;
 
-            if (!string.IsNullOrEmpty(equiptmentType) &&
-                serialNumber >= 0 &&
-                id >= 0)
+        public ICollection<Equipment> GetAll()
+        {
+            var commandString = $"SELECT * FROM dbo.Equipment";
+            var command = new SqlCommand(commandString, Connection);
+
+            var result = new List<Equipment>();
+
+            using (var reader = command.ExecuteReader())
             {
-                result = new Equiptment()
+                while (reader.Read())
                 {
-                    EquiptmentType = equiptmentType,
-                    SerialNumber = serialNumber,
-                    Id = Id
-                };
-                equiptments.Add(result);
+                    var dateString = reader[3].ToString();
+                    var registrationDate = DateTime.UnixEpoch;
+                    if (dateString != null)
+                        registrationDate = DateTime.Parse(dateString);
+
+                    result.Add(new Equipment(
+                        id: Convert.ToInt32(reader[0].ToString()),
+                        equipmentTypeId: Convert.ToInt32(reader[1].ToString()),
+                        serialNumber: Convert.ToInt32(reader[2].ToString()),
+                        registrationDate: registrationDate,
+                        locationId: Convert.ToInt32(reader[4].ToString())
+                        ));
+                }
             }
-            else
-                throw (new ArgumentException("not all arguments are Valid"));
             return result;
         }
-        public Equiptment Get(int Id)
-        {
-            Equiptment equiptment = null;
 
-            foreach (Equiptment e in equiptments)
-            {
-                if (e.Id == Id)
-                {
-                    result = e;
-                    break;
-                }
-            }
-            return result;
-        }
-        public List<Equiptment> GetAll()
+        public bool Update(Equipment equipment)
         {
-            return equiptments;
-        }
-        public void Update(string equiptmentType, int serialNumber, int Id)
-        {
-            Equiptment foundEquiptment = this.Get(Id);
 
-            if (foundEquiptment != null)
-            {
-                if (!string.IsNullOrEmpty(equiptmentType) &&
-                    serialNumber <= 0)
-                {
-                    if (foundEquiptment.EquiptmentType != equiptmentType)
-                        foundEquiptment.EquiptmentType = equiptmentType;
-                    if (foundEquiptment.SerialNumber != serialNumber)
-                        foundEquiptment.SerialNumber = serialNumber;
-                }
-                else
-                    throw (new ArgumentException("Not all arguments for equiptment are valid"));
-            }
-            else
-                throw new ArgumentException("Equiptment with id = " + Id + " not found"));
+            var existing = Get(equipment.Id);
+
+            if (existing == null)
+                return false;
+
+            var commandString = "UPDATE Equipment" +
+                 $"SET EquipmentTypeID = {equipment.EquipmentTypeId}, SerialNumber = {equipment.SerialNumber}, LocationID = {equipment.LocationId}" +
+                 $"WHERE ID = {equipment.Id};";
+            var command = new SqlCommand(commandString, Connection);
+            command.ExecuteReader().Close();
+
+            return true;
         }
-        public void Remove(int Id)
+
+        public void Dispose()
         {
-            Equiptment foundEquiptment = this.Get(Id);
-            if (foundEquiptment != null)
-                equiptments.Remove(foundEquiptment);
-            else
-                throw (new ArgumentException("Equiptment with id = " + Id + "not found"));
+            Connection.Close();
         }
     }
 }
